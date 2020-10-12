e02d75fa2fba4951cf1c1ab14c45cef7effe2e6d
Viktor D <Lespaul1989@gmx.de>
2020-10-12 21:47:57 +0200

    Leichte Veränderungen; Unterstuetzung von parameter modes soll nun implementiert werden

--- Day5.py
+++ Day5.py
@@ -1,43 +1,50 @@
 import csv
-input = None
+puzzleInput = None
 
 with open("C:\\Users\\ViktorDingelmaier\\OneDrive - tu-dresden.de\\Coding\\Python\\Projects\\Advent_of_code\\Input_Day5.txt", newline="") as file:
     reader = csv.reader(file, delimiter=",")
-    def get_input():
+    def get_puzzleInput():
         for line in reader:
             return line
     
-    input = get_input()
+    puzzleInput = get_puzzleInput()
 
-input = [int(number) for number in input]
+puzzleInput = [int(number) for number in puzzleInput]
 
-input[1] = 12
-input[2] = 2
+puzzleInput[1] = 12
+puzzleInput[2] = 2
 
-print("Original:\n", input)
+#print("Original:\n", puzzleInput)
 i = 0
-while i<len(input)-4:
-    opcode = input[i]
-    inputValue = -1000
+#while i<len(puzzleInput):
+for instruction in puzzleInput:
+    opcode = puzzleInput[i]
     if opcode != 99:
         if opcode == 1:
-            input[input[i+3]] = input[input[i+1]] + input[input[i+2]]
+            """" Opcode 1: addition - the first two numbers (called puzzleInputs) after the opcode indicate the positions from which the puzzleInput values should be read. 
+            The third one indicates the position at which the output should be stored """
+            puzzleInput[puzzleInput[i+3]] = puzzleInput[puzzleInput[i+1]] + puzzleInput[puzzleInput[i+2]]
             i += 4
         elif opcode == 2:
-            input[input[i+3]] = input[input[i+1]] * input[input[i+2]]
+            """ Opcode 2: multiplication -  multiply the two puzzleInput numbers and store the result at the output position given by the third number"""
+            puzzleInput[puzzleInput[i+3]] = puzzleInput[puzzleInput[i+1]] * puzzleInput[puzzleInput[i+2]]
             i += 4
         elif opcode == 3:
-            input[i+1] = inputValue
+            """ Opcode 3: save a given puzzleInput at the position specified by the following number """
+            systemID = int(input("Please enter the ID of the system to test: "))
+            puzzleInput[i+1] = systemID
             i += 2
         elif opcode == 4:
-            print("Opcode 4 print:", input[i+1])
+            """ Opcode 4: output the value at the position specified by the following number """
+            print("Opcode 4 print:", puzzleInput[i+1])
             i += 2
-        """ else:
-            print("Opcode '{}' could not be recognized".format(opcode)) """
+        else:
+            i += 4
+            print("Opcode '{}' could not be recognized".format(opcode))
 
     else:
-        print("Program ended at position", i, "because the value", input[i], "occured")
+        #If opcode 99 is encountered, halt the program
+        print("Program ended at position", i, "because the value", puzzleInput[i], "occured")
         break
-    
 
-print("New:\n", input)
+print("New:\n", puzzleInput)
---
+++ Sandbox.py
@@ -0,0 +1,8 @@
+i = 0
+
+while i <= 5:
+    if i < 3:
+        print(i)
+        i += 1
+    else: 
+        i += 1
